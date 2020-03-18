49b0d7733adad4b35c05b278357ba93f284b4e4d
wasuken <wevorence@gmail.com>
2020-03-18 16:42:02 +0900

    add test.

--- test/controllers/net_packets_controller_test.rb
+++ test/controllers/net_packets_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class NetPacketsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get root" do
+    get "/"
+    assert_select "h2", "index"
+    assert_response :success
+  end
+  test "get api root" do
+    get "/api/v1/net_packets"
+    assert_response :success
+  end
 end
